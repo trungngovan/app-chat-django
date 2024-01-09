90beebb1ce0e5313f5059afa2c4c82825c374dc6
Ngô Văn Trung <trung2601.it@gmail.com>
2024-01-09 15:02:27 +0700

    [init]: init project app chat django

---
+++ app_chat/env.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_CODE_DIR = Path(__file__).resolve().parent.parent
+
+# Load variables from .env file
+load_dotenv(BASE_CODE_DIR / '.env')
+
+
+# DB
+DB_HOST = os.getenv('DB_HOST', None)
+assert DB_HOST
+
+DB_PORT = os.getenv('DB_PORT', None)
+assert DB_PORT
+
+DB_NAME = os.getenv('DB_NAME', None)
+assert DB_NAME
+
+DB_USER = os.getenv('DB_USER', None)
+assert DB_USER
+
+DB_PASSWORD = os.getenv('DB_PASSWORD', None)
+assert DB_PASSWORD
+
